9ba50720d6ef072801f1dfefcd3f657ed8d7851d
Mash J Prime <mashjprime@gmail.com>
2020-10-02 16:36:49 +0800

    Completed Chapter 1 projects

--- chapter1.js
+++ chapter1.js
@@ -1,24 +1,38 @@
 console.log("Hello world");
 
-// Looping a triangle
-let star = "*";
-for (let x = 0; x < 7; x++) {
-  console.log(star);
-  star += "*";
+function LoopingTriangle(y) {
+    let star = "*";
+    for (let x = 0; x < y; x++) {
+      console.log(star);
+      star += "*";
+    }
 }
 
-// FizzBuzz
-for (let x = 0; x <= 100; x++) {
-  if (x % 3 == 0) {
-    console.log("Fizz");
-  } else if (x % 5 == 0) {
-    console.log("Buzz");
-  } else console.log(x);
+
+function FizzBuzz() {
+    for (let x = 0; x <= 100; x++) {
+        if (x % 3 == 0) {
+          console.log("Fizz");
+        } else if (x % 5 == 0) {
+          console.log("Buzz");
+        } else console.log(x);
+      }
 }
 
-// Chessboard
+let chessOutput = "";
 function Chessboard(x, y) {
-
+  for (let depth = 0; depth < y; depth++) {
+    if (depth % 2 === 0) {
+      chessOutput += "  ";
+    }
+    for (let width = 0; width < x; width++) {
+      chessOutput += "#  ";
+    }
+    chessOutput += "\n";
+  }
+  console.log(chessOutput);
 }
 
-Chessboard(8,8);
+FizzBuzz();
+LoopingTriangle(20);
+Chessboard(8, 8);
